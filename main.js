8ea1447174d14a65b7bef3d7c9233fd2708faefb
Rezlen <soheilyreza@yahoo.com>
2022-05-15 20:13:15 +0100

    My First Commit

---
+++ main.js
@@ -0,0 +1,77 @@
+﻿// TODO: Add your import statements here.
+import { getRoles, getCompanies } from './modules/salaryData.js';
+//In main.js, import the four functions from workAroundModule.js.
+import {getAverageSalaryByRole, getAverageSalaryByCompany, getSalaryAtCompany, getIndustryAverageSalary} from './modules/workAroundModule.js';
+import formatNumber from './modules/utilities.js'
+// TODO: Get the companies and roles using the salaryData module.
+const companies = getCompanies();
+const roles = getRoles();
+
+/*Currently, this function is being called twice with the variables companies and roles as the first arguments. However, each of these variables are assigned empty arrays.
+
+Instead, you will use the getRoles() and getCompanies() functions from salaryData.js to initialize these variables.
+
+First, at the top of main.js, use ES6 named import syntax to import getRoles and getCompanies from salaryData.js. Check the file system to determine the relative path from main.js.*/
+// Create input buttons for every company and role represented in the data.This creates RADIOS BUTTON
+renderInputButtons(companies, 'company');
+renderInputButtons(roles, 'role');
+
+// This function will create a new <section> with radio
+// inputs based on the data provided in the labels array.
+function renderInputButtons(labels, groupName) {
+  const container = document.createElement('section');
+  container.setAttribute('id', `${groupName}Inputs`);
+
+  let header = document.createElement('h3');
+  header.innerText = `Select a ${groupName}`;
+  container.appendChild(header);
+
+  labels.forEach(label => { // For each label...
+    // Create the radio input element.
+    let divElement = document.createElement('div');
+    divElement.setAttribute('class', 'option');
+
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'radio');
+    inputElement.setAttribute('name', groupName);
+    inputElement.setAttribute('value', label);
+    divElement.appendChild(inputElement);
+
+    // Create a label for that radio input element.
+    let labelElement = document.createElement('label');
+    labelElement.setAttribute('for', label);
+    labelElement.innerText = label;
+    divElement.appendChild(labelElement);
+
+    // Update the results when the input is selected.
+    inputElement.addEventListener('click', updateResults);
+
+    container.appendChild(divElement);
+  });
+
+  document.querySelector('main').prepend(container);
+}
+
+function updateResults() {
+  // Get the current selected company and role from the radio button inputs.
+  const company = document.querySelector("input[name='company']:checked").value;
+  const role = document.querySelector("input[name='role']:checked").value;
+
+  // If either the company or role is unselected, return.
+  if (!company || !role) { return; }
+
+  // Use the workAroundModule functions to calculate the needed data.
+  const averageSalaryByRole = formatNumber(getAverageSalaryByRole(role));
+  const averageSalaryByCompany = formatNumber(getAverageSalaryByCompany(company));
+  const salary = formatNumber(getSalaryAtCompany(role, company));
+  const industryAverageSalary = formatNumber(getIndustryAverageSalary());
+
+  // Render them to the screen.
+  document.getElementById('salarySelected').innerText = `The salary for ${role}s at ${company} is \$${salary}`;
+  document.getElementById('salaryAverageByRole').innerText = `The industry average salary for ${role} positions is \$${averageSalaryByRole}`;
+  document.getElementById('salaryAverageByCompany').innerText = `The average salary at ${company} is \$${averageSalaryByCompany}`;
+  document.getElementById('salaryAverageIndustry').innerText = `The average salary in the Tech industry is \$${industryAverageSalary}`;
+}
+
+
+
---
+++ modules/workAroundModule.js
@@ -0,0 +1,54 @@
+﻿/* 
+Open up workAroundModule.js where you will find four functions that each calculate a different data value that we want to display. They are currently incomplete.
+
+To complete these four functions, you will need some data from salaryData.js.
+
+Import the functions getDataByRole() and getDataByCompany() from salaryData.js using named import syntax.
+Import salaryData from salaryData.js using the default import syntax.
+Note: The reason these functions are in a separate module from salaryData.js is to achieve separation of concerns. salaryData.js is concerned only with providing access to raw data while workAroundModule.js is concerned with digging into the data to find precise values.
+*/
+
+// TODO: Add your import statements here. 
+// Named import syntax:
+import { getDataByRole, getDataByCompany } from './salaryData.js';
+// Default import syntax:
+import salaryData from './salaryData.js';
+
+// Replace the empty array with the appropriate imported function/value
+const getAverageSalaryByRole = role => {
+  const roleData = getDataByRole(role);
+  const salariesOfRole = roleData.map(obj => obj.salary);
+  return calculateAverage(salariesOfRole);
+};
+
+// Replace the empty array with the appropriate imported function/value
+const getAverageSalaryByCompany = company => {
+  const companyData = getDataByCompany(company);
+  const salariesAtCompany = companyData.map(obj => obj.salary);
+  return calculateAverage(salariesAtCompany);
+};
+
+// Replace the empty array with the appropriate imported function/value
+const getSalaryAtCompany = (role, company) => {
+  const companyData = getDataByCompany(company);
+  const roleAtCompany = companyData.find(obj => obj.role === role);
+  return roleAtCompany.salary;
+};
+
+// Replace the empty array with the appropriate imported function/value
+const getIndustryAverageSalary = () => {
+  const allSalaries = salaryData.map(obj => obj.salary);
+  return calculateAverage(allSalaries);
+};
+
+
+// Helper Function. Do not edit.
+// Note: This function does not need to be exported since it is only used by the functions contained within this module.
+function calculateAverage(arrayOfNumbers) {
+  let total = 0;
+  arrayOfNumbers.forEach(number => total += number);
+  return (total / arrayOfNumbers.length).toFixed(2);
+};
+
+// Export the four functions using ES6 named export syntax.
+export {getAverageSalaryByRole, getAverageSalaryByCompany, getSalaryAtCompany, getIndustryAverageSalary};
